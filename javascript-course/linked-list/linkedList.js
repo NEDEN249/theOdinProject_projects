4348a75bf0c1322e532f5cf6d7801a0df16970d3
Nathan <61109926+NEDEN249@users.noreply.github.com>
2023-12-18 19:16:55 +0800

    implement basic linked list functionality

---
+++ javascript-course/linked-list/linkedList.js
@@ -0,0 +1,147 @@
+function Node(value){
+    return {
+        value: value,
+        next: null
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    getHead() { return this.head; }
+    getTail() { return this.tail; }
+    getLength() { return this.length; }
+    append(value){
+        const newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+            this.length++;
+            return newNode;
+        }
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+        return newNode;
+    }
+    prepend(value){
+        const newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+            this.length++;
+            return newNode;
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return newNode;
+    }
+    pop(){
+        if(!this.head){ //no elements to pop
+            return null;
+        }
+        if(this.head === this.tail){ //implies removing this node will leave an empty list
+            const node = this.head; //pop return value
+            this.head = null; 
+            this.tail = null;
+            this.length--;
+            return node;
+        }
+        let currentNode = this.head; //initial node
+        while(currentNode.next !== this.tail){ //this will eventually lead to currentNode being the second to last node
+            currentNode = currentNode.next;
+        }
+        const node = this.tail; //pop return value
+        currentNode.next = null; //remove the last node from the list
+        this.tail = currentNode; //change the tail to what was the second last node (now last node)
+        this.length--;
+        return node; //return the popped node
+    }
+    contains(value){
+        let currentNode = this.head;
+        while(currentNode.next !== null){
+            if(currentNode.value == value){
+                return true
+            }
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+    at(index){
+        if (index < 0 || index >= this.length){
+            return null;
+        }
+        let currentNode = this.head;
+        let currentIndex = 0;
+        while(currentNode.next !== null){
+            console.log(currentNode.value);
+            if(currentIndex === index){
+                return currentNode.value;
+            }
+            else{
+                currentIndex++;
+                currentNode = currentNode.next;
+            }
+        }
+        return false;
+    }
+    find(value){
+        let currentNode = this.head;
+        let index = 0;
+        while(currentNode.next !== null){
+            if(currentNode.value === value){
+                return index;
+            }
+            else{
+                index++;
+                currentNode = currentNode.next;
+            }
+        }
+        return null;
+    }
+    toString(){
+        let currentNode = this.head;
+        let string = '';
+        while(currentNode.next !== null){
+            string += currentNode.value + ' -> ';
+            currentNode = currentNode.next;
+        }
+        string += currentNode.value;
+        return string;
+    }
+
+    // insertAt(value, index){
+
+    // }
+
+    // removeAt(index){
+
+    // }
+    
+}
+
+const myLinkedUpList = new LinkedList();
+
+myLinkedUpList.append(1);
+myLinkedUpList.append(2);
+myLinkedUpList.append(3);
+console.log(myLinkedUpList.toString());
+myLinkedUpList.prepend(4);
+console.log(myLinkedUpList.toString());
+myLinkedUpList.pop();
+console.log(myLinkedUpList.toString());
+console.log(myLinkedUpList.contains(3));
+console.log(myLinkedUpList.getTail());
+console.log(myLinkedUpList.getHead());
+console.log(myLinkedUpList.at(2));
+console.log(myLinkedUpList.contains(1));
+console.log(myLinkedUpList.find(4));
+
+
+
+
